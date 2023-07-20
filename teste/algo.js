172d543253a14f78ea8c8e8a8175d45e46f95d9a
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2023-07-20 11:23:55 -0300

    Add files via upload

---
+++ teste/algo.js
@@ -0,0 +1,27 @@
+// alert("Hello Word")
+
+// console.log("Hello Word");
+
+// document.write("Hello Word")
+
+// var num1 = 1
+
+// document.write("\n")
+
+// document.write(num1)
+
+var saida1 ="global"
+
+// function metodo(){
+
+//     var saida2 = "local"
+
+//     return saida2
+
+
+// }
+
+ document.write(saida1)
+
+// document.write(metodo())
+
